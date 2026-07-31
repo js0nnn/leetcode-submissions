d8c135ee728154c8cdc6573704a2a656ba135496
Jaison Edward <133663586+js0nnn@users.noreply.github.com>
2026-07-31 19:53:32 +0530

    Time: 3 ms (58.54%), Space: 44.5 MB (55.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,37 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
+
+        if(s.length() == 1)
+            return true;
+        
         s = s.toLowerCase();
-        for(char ch : s.toCharArray()) {
-            if(Character.isLetter(ch) || Character.isDigit(ch)) 
-                sb.append(ch);
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i <= j) {
+
+            if(!(Character.isLetter(s.charAt(i)) || Character.isDigit(s.charAt(i)))){
+                i++;
+                continue;
+            }
+
+            if(!(Character.isLetter(s.charAt(j)) || Character.isDigit(s.charAt(j)))){
+                j--;
+                continue;
+            }
+
+            if(s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+            } else if(i == j) {
+                i++;
+                j--;
+            } else {
+                return false;
+            }
         }
 
-        // if(sb.length() <= 1)
-        //     return false;
-
-        System.out.println(sb);
-
-        return sb.toString().equals(sb.reverse().toString());
+        return true;
     }
 }
