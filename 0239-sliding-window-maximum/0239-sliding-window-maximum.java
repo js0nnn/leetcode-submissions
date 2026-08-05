cee603bfc65f450594f9815fde43adefb29f2187
Jaison Edward <133663586+js0nnn@users.noreply.github.com>
2026-08-05 12:35:54 +0530

    Time: 21 ms (98.04%), Space: 146.8 MB (58.02%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,5 +1,13 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
+    static{
+        int [] ans = new int[]{5,6,7};
+
+        for(int i =0; i< 301;i++){
+            maxSlidingWindow(ans,2);
+        }
+       
+    }
+    public static int[] maxSlidingWindow(int[] nums, int k) {
         
         int res[] = new int[nums.length - k + 1]; 
         int ctr = 0;
