6224471188631b7fd4f58400fe0350c25b9a46a3
Jaison Edward <133663586+js0nnn@users.noreply.github.com>
2026-08-07 13:02:23 +0530

    Time: 0 ms (100%), Space: 44 MB (11.36%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,30 +1,35 @@
-class Solution {
+public class Solution {
   public int search(int[] nums, int target) {
-    int l = 0;
-    int r = nums.length - 1;
+    int l = 0, r = nums.length - 1;
+
+    while (l < r) {
+      int m = (l + r) / 2;
+      if (nums[m] > nums[r]) {
+        l = m + 1;
+      } else {
+        r = m;
+      }
+    }
+
+    int pivot = l;
+    l = 0;
+    r = nums.length - 1;
+
+    if (target >= nums[pivot] && target <= nums[r]) {
+      l = pivot;
+    } else {
+      r = pivot - 1;
+    }
 
     while (l <= r) {
-
-      int mid = (l + r) / 2;
-
-      if (nums[mid] == target) {
-        return mid;
-      }
-
-      if (nums[l] <= nums[mid]) {
-        if (target > nums[mid] || target < nums[l]) {
-          l = mid + 1;
-        } else {
-          r = mid - 1;
-        }
+      int m = (l + r) / 2;
+      if (nums[m] == target) {
+        return m;
+      } else if (nums[m] < target) {
+        l = m + 1;
       } else {
-        if (target < nums[mid] || target > nums[r]) {
-          r = mid - 1;
-        } else {
-          l = mid + 1;
-        }
+        r = m - 1;
       }
-
     }
 
     return -1;
